15f0557602cc7600b5cc56eae508d065857f4607
rajan0031 <109027109+rajan0031@users.noreply.github.com>
2024-06-12 22:37:31 +0530

    Update PostSavingToUserProfileApiRoutes.js

--- frontend/utils/PostSavingToUserProfileApiRoutes/PostSavingToUserProfileApiRoutes.js
+++ frontend/utils/PostSavingToUserProfileApiRoutes/PostSavingToUserProfileApiRoutes.js
@@ -1,5 +1,5 @@
 // here we are defining the host of the main api
-export const host = 'http://localhost:8080';
+export const host = 'https://rksocial.onrender.com';
 
 // this is the apir outef for the sending the for the frontend data to the backedn
 // saving the ppost to the user profile
