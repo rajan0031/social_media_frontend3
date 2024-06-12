323b2e0f3cbf23ff4809979e7759fa2cbef7c305
rajan0031 <109027109+rajan0031@users.noreply.github.com>
2024-06-12 22:36:40 +0530

    Update CommentsReplyApiRoutes.js

--- frontend/utils/CommentsReplyApiRoutes/CommentsReplyApiRoutes.js
+++ frontend/utils/CommentsReplyApiRoutes/CommentsReplyApiRoutes.js
@@ -1,5 +1,5 @@
 // here we are defining the host of the main api
-export const host = 'http://localhost:8080';
+export const host = 'https://rksocial.onrender.com';
 
 // add a reply to a  comments to the comments in the comment sections
 export const addReplyToComment = `${host}/addreplytocomment`;
