e881248b0212004953bcea2e53d408f512c5267d
rajan0031 <109027109+rajan0031@users.noreply.github.com>
2024-06-12 22:37:15 +0530

    Update GroupMessagesApiRoutes.js

--- frontend/utils/GroupMessagesApiRoutes/GroupMessagesApiRoutes.js
+++ frontend/utils/GroupMessagesApiRoutes/GroupMessagesApiRoutes.js
@@ -1,6 +1,6 @@
 // making the backend end point 
 // here we are defining the host of the main api
-export const host = 'http://localhost:8080';
+export const host = 'https://rksocial.onrender.com';
 
 // making a frontend end point for the adding the message in the database
 
