cb5d862b617579297908f53ea9d1d6c597e47851
rajan0031 <109027109+rajan0031@users.noreply.github.com>
2024-06-12 22:39:08 +0530

    Update searchResultsApis.js

--- frontend/utils/searchResultsApis/searchResultsApis.js
+++ frontend/utils/searchResultsApis/searchResultsApis.js
@@ -1,5 +1,5 @@
 // here we are defining the host of the main api
-export const host = 'http://localhost:8080';
+export const host = 'https://rksocial.onrender.com';
 
 // api routes for the searching the posts and blogs
 
